7aad7e0a90137ac625f4544e984aa1e28c6d54ff
Joel Mulimba <nadebu@gmail.com>
2026-02-10 12:46:42 +0000

    remove unncessary commas

--- terraform/snowflake/roles.locals.tf
+++ terraform/snowflake/roles.locals.tf
@@ -2,10 +2,10 @@ locals {
   snowflake_roles = {
     LOADER = {
       comment = "Used for data loading operations"
-    },
+    }
     TRANSFORMER = {
       comment = "Used by developers for transforming and modelling data"
-    },
+    }
     READER = {
       comment = "Read only role for query curated eg using BI tools"
     }
